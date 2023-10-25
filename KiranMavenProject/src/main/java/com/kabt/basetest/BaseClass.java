89224dede1ed4806025acce1a440428e1ee7d40b
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2023-10-25 17:45:12 +0530

    Added BaseClass

--- KiranMavenProject/src/main/java/com/kabt/basetest/BaseClass.java
+++ KiranMavenProject/src/main/java/com/kabt/basetest/BaseClass.java
@@ -1,5 +1,25 @@
 package com.kabt.basetest;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
+public class BaseClass {
+	public static WebDriver driver;
+	@Parameters("browserName")
+	@BeforeClass
+	public void browserSetup(@Optional("chrome") String bname) {
+		if (bname.equalsIgnoreCase("chrome")) {
+			driver=new ChromeDriver();
+			Reporter.log("chrome browser launched",true);
+		}else if(bname.equalsIgnoreCase("firefox")) {
+			driver=new FirefoxDriver();
+			Reporter.log("firefox browser launched",true);
+		}
+		driver.manage().window().maximize();
+	}
 }
--- KiranMavenProject/src/test/java/shoppingcart/TC_003_Test.java
+++ KiranMavenProject/src/test/java/shoppingcart/TC_003_Test.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class TC_003_Test {
-	public static WebDriver driver;
+import com.kabt.basetest.BaseClass;
+
+public class TC_003_Test extends BaseClass{
+	
 	@Test
 	public void demo3() {
 		Reporter.log("TC_003_Test executed",true);
