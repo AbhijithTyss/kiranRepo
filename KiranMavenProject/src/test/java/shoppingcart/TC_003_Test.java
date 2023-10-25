9c88421e9b0fd459ff278c5744c1c11c521593cc
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2023-10-25 17:39:15 +0530

    created instance of ChromeDriver

--- KiranMavenProject/src/test/java/shoppingcart/TC_003_Test.java
+++ KiranMavenProject/src/test/java/shoppingcart/TC_003_Test.java
@@ -1,11 +1,15 @@
 package shoppingcart;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class TC_003_Test {
+	public static WebDriver driver;
 	@Test
 	public void demo3() {
 		Reporter.log("TC_003_Test executed",true);
+		driver=new ChromeDriver();
 	}
 }
